bd9e1b9fcd587f605831623d75fd3c2eae5f1ba0
iOneDev305 <sonlyhorng.ione@gmail.com>
2025-03-07 16:03:37 +0700

    add compo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,18 @@
-<template>
-    <div>
-        <slot />
-    </div>
-</template>
-
 <script setup>
-import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
-
-
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar.vue";
+import Menubar from "~/components/Menubar.vue";
 </script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <main class="flex flex-col">
+      <div class="flex items-center gap-2 p-2">
+        <SidebarTrigger />
+        <Menubar />
+      </div>
+      <slot />
+    </main>
+  </SidebarProvider>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import AppSidebar from "@/components/AppSidebar.vue";
-</script>
-
 <template>
-  <SidebarProvider>
-    <AppSidebar />
-    <main>
-      <SidebarTrigger />
-      <slot />
-    </main>
-  </SidebarProvider>
+  
 </template>
